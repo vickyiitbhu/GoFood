a9a452c4e7967d4ef3a7b3a2b82f7513d0891ebb
vickyiitbhu <vicky.bijarniya.civ20@itbhu.ac.in>
2023-10-05 18:55:35 +0530

    3000 to link

--- src/screens/Myorders.js
+++ src/screens/Myorders.js
@@ -8,9 +8,9 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:5000/api/myorderdata", {
+        await fetch("https://go-food-mu.vercel.app/api/myorderdata", {
             // credentials: 'include',
-            // Origin:"http://localhost:3000/login",
+            // Origin:"https://go-food-mu.vercel.app/login",
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
