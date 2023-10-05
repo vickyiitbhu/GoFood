9126ff8eb3bdc982284ef0e2a8342cff1c5406cf
vickyiitbhu <vicky.bijarniya.civ20@itbhu.ac.in>
2023-10-05 18:49:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const port = 5000
 const mongoDB=require('./db')
 
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin","https://go-food-mu.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type,Accept"
